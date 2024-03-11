a1090b22df068303cafa69fd4b322c47fdb88e17
ailtonbsj <ailton.ifce@gmail.com>
2024-03-11 12:00:00 -0300

    changed vortex0

--- 08-vortex/vortex_00/level_00.py
+++ 08-vortex/vortex_00/level_00.py
@@ -3,18 +3,12 @@
 from socket import *
 from struct import *
 
-clientSocket = socket()
+clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(('vortex.labs.overthewire.org', 5842))
-res0 = clientSocket.recv(4)
-res1 = clientSocket.recv(4)
-res2 = clientSocket.recv(4)
-res3 = clientSocket.recv(4)
-unsigneds = (
-    unpack('I', res0)[0],
-    unpack('I', res1)[0],
-    unpack('I', res2)[0],
-    unpack('I', res3)[0]
-    )
+unsigneds = []
+for i in range(0,4):
+    unsigned = clientSocket.recv(4)
+    unsigneds.append(unpack('I', unsigned)[0])
 print(unsigneds)
 u_sum = sum(unsigneds).to_bytes(8, 'little')
 print(u_sum)
